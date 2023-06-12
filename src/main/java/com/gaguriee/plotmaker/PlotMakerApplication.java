f8148d2f5fa8e88e96276f592aa05833d86b1df5
gaguriee <74501631+gaguriee@users.noreply.github.com>
2023-06-12 14:54:04 +0900

    set environment

---
+++ src/main/java/com/gaguriee/plotmaker/PlotMakerApplication.java
@@ -0,0 +1,13 @@
+package com.gaguriee.plotmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlotMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlotMakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gaguriee/plotmaker/PlotMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaguriee.plotmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlotMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
