19f5edddc0708235c379978eaac8aed5b685c394
gaguriee <74501631+gaguriee@users.noreply.github.com>
2023-06-12 14:54:29 +0900

    add config file for openAI gpt

---
+++ src/main/java/com/gaguriee/plotmaker/config/ChatGptConfig.java
@@ -0,0 +1,16 @@
+package com.gaguriee.plotmaker.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatGptConfig {
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer ";
+    public static final String MODEL = "gpt-3.5-turbo";
+    public static final Integer MAX_TOKEN = 2000;
+    public static final Double TEMPERATURE = 0.0;
+    public static final Double TOP_P = 1.0;
+    public static final String MEDIA_TYPE = "application/json; charset=UTF-8";
+    public static final String URL = "https://api.openai.com/v1/chat/completions";
+
+}
